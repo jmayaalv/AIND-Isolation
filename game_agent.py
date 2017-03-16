090441793dc5a65f3fb183f190a92d35f1a2666e
Juan E. Maya <jmayaalv@gmail.com>
2017-03-16 10:22:07 +0100

    multiple heuristic

--- game_agent.py
+++ game_agent.py
@@ -15,29 +15,26 @@ class Timeout(Exception):
     pass
 
 
-def custom_score(game, player):
+def heuristic_weighted_moves(game, player):
     """Calculate the heuristic value of a game state from the point of view
-    of the given player.
+      of the given player.
 
-    Note: this function should be called from within a Player instance as
-    `self.score()` -- you should not need to call this function directly.
 
-    Parameters
-    ----------
-    game : `isolation.Board`
-        An instance of `isolation.Board` encoding the current state of the
-        game (e.g., player locations and blocked cells).
+      Parameters
+      ----------
+      game : `isolation.Board`
+          An instance of `isolation.Board` encoding the current state of the
+          game (e.g., player locations and blocked cells).
 
-    player : object
-        A player instance in the current game (i.e., an object corresponding to
-        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+      player : object
+          A player instance in the current game (i.e., an object corresponding to
+          one of the player objects `game.__player_1__` or `game.__player_2__`.)
 
-    Returns
-    -------
-    float
-        The heuristic value of the current game state to the specified player.
-    """
-
+      Returns
+      -------
+      float
+          The heuristic value of the current game state to the specified player.
+      """
     # active player moves
     if game.is_loser(player):
         return float("-inf")
@@ -48,6 +45,10 @@ def custom_score(game, player):
     own_moves = len(game.get_legal_moves(player))
     opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
     return float(own_moves - opp_moves)
+
+
+# Set the heuristic function
+custom_score = heuristic_weighted_moves
 
 
 class CustomPlayer:
@@ -140,7 +141,7 @@ class CustomPlayer:
         search_fn = self.minimax if self.method == 'minimax' else self.alphabeta
         depth = 1
         try:
-            while self.iterative: # FIXME in case iterative is false. how?
+            while self.iterative:  # FIXME in case iterative is false. how?
                 score, move = search_fn(game, depth)
                 depth += 1
                 if (score, move) > (best_score, best_move):
---
+++ heuristic_analysis.md
@@ -0,0 +1,7 @@
+# Heuristics Analysis
+
+## Synopsis
+
+
+The following analysis displays the performance of 3 heuristics for the Isolation game. Along the 3 heuristics presented below the performance
+  will 
